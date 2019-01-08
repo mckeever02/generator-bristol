ded4f9ca8de592a91e59eff7581cafc78d7621b6
Michael <mckeever02@gmail.com>
2019-01-08 11:46:09 +0000

    Add uncss

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,7 +1,17 @@
+const uncssConfig = {
+  html: [
+    'https://generatorbuilding.co.uk/',
+    // Your entire sitemap added manually
+    // or some other way if you’re clever (wget is handy for this).
+  ],
+  // ignore: [/\.dark/, /\.active/]
+};
+
 // eslint-disable-next-line no-unused-vars
 module.exports = ({ file, options, env }) => ({
   parser: 'postcss-scss',
   plugins: {
+    'postcss-uncss': env === 'production' ? uncssConfig : false,
     'postcss-import': {},
     autoprefixer: {},
     cssnano: env === 'production' ? {} : false,
